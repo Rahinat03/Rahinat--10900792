ca383d75c101507830d2acae2c66bf5e0cd7d417
Rahinat03 <rahinakohusnah@gmail.com>
2023-07-18 14:53:49 +0000

    First commit

--- src/main/java/com/example/inventorymanagementsystem/Admin.java
+++ src/main/java/com/example/inventorymanagementsystem/Admin.java
@@ -12,7 +12,6 @@ public class Admin extends javax.swing.JFrame {
 
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
